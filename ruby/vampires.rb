e55b4f4aa9ee6a3dd46c69f27365b01b92279ae1
Joshua McDonald <joshua_mcdonald@me.com>
2016-05-28 22:14:15 -0500

    Add Release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,3 +10,23 @@ garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
 
+age <= 120
+
+if age <= 120
+	puts "Probably not a vampire."
+else age >= 121
+	puts "Probably a vampire."
+end
+
+if garlic_bread == "yes"
+	puts "Probably not a vampire."
+elsif garlic_bread == "no"
+	puts "Probably a vampire."
+else puts "Probably a vampire."
+end
+
+if insurance == "yes"
+	puts "Probably not a vampire."
+else puts "Probably a vampire."
+end
+		
