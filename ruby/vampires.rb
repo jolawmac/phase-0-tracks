74988125c5a153759403974735d715a6f5fc5c70
Joshua McDonald <joshua_mcdonald@me.com>
2016-05-30 20:19:24 -0500

    Update Release 4 Loops

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,12 +1,13 @@
-# Questions asked: 
+# Release 3: 
 
 puts "How many employees will be processed?"
 employee = gets.chomp.to_i
-
-# loop 
-
 i = 0
-while i < employee 
+while i < employee
+
+
+
+# Questions asked:
 
 puts "What is your name?"
 name = gets.chomp.to_s
@@ -23,24 +24,36 @@ garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance? (yes or no)"
 insurance = gets.chomp
 
-# Other Variables å
+# Other Variables
 
 current_year = 2016
 
+# Release 4
+
+allergies = ""
+	until allergies == "done" || allergies == "sunshine"
+		puts "Please list any allergies you may have. Type 'done' when you are finished."
+		allergies = gets.chomp
+	end
+
 # Results 
+
  	if name == "Drake Cula" || name == "Tu Fang"
  		puts "Definitely a vampire."
 
-	elsif ((current_year - year) == age) && (garlic_bread == "yes" || insurance == "yes"
-		puts "Probably not a vampire.")
+	elsif ((current_year - year) == age) && (garlic_bread == "yes" || insurance == "yes")
+		puts "Probably not a vampire."
 	
-	elsif ((current_year - year) != age) && (garlic_bread == "no" || insurance == "waived"
-		puts "Probably a vampire.")
+	elsif ((current_year - year) != age) && (garlic_bread == "no" || insurance == "no" || allergies == "sunshine")
+		puts "Probably a vampire."
 
 	elsif ((current_year - year) != age) && garlic_bread == "no" && insurance == "no"
 		puts "Almost certainly a vampire."
 
 	else 
 		puts "Results inconclusive."
+	end
+
+i += 1 
+
 end
-end 
