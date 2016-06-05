3a4fc7dac38f189150b183eb36e4841d115a6268
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-05 09:38:15 -0500

    Add if else statement to create boolean

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,10 +3,11 @@
 # Ask the client how many children they have
 # Ask for the desired theme
 # List available themes and give the choice to chose one or several
-
+	# Allow for 'yes' or 'no' answers
+	# Creat a boolean method using the if/else method to make 'yes' or 'no' into true or false statements
 # Convert input to the correct data type 
 # Creat empty hash
-# If/else method 
+# Put get.chomps into hash
 # Print the results
 # Allow the user to update or make changes to input
 	# If yes ask what updates or changes need to be made
@@ -14,39 +15,48 @@
 # Print the final results 
 
 
-# Method for 'yes' or 'no' answer in questions below
+# Method to define boolean
 
-def theme_chosen(str)
-	str.downcase
-	if st == "yes" || str == "y"
-		puts true
-	elsif str == "no" || str == "n"
-		puts false
-	else 
-		puts "No answer"
+def theme(key)
+	if key == "yes"
+		reply = true
+	elsif key == "no"
+		reply = false
+	else
+		reply = key
+end
 end 
 
 # Empty Hash 
-interior_design = {}
+interior_design = Hash.new(0)
 
 # Questions for clients:
 
 puts "What is the client's name?"
-clients_name[:name] = gets.chomp
+interior_design[:name] = gets.chomp
 
 puts "What is the client's age?"
-clients_age[:age] = gets.chomp.to_i
+interior_design[:age] = gets.chomp.to_i
 
 
 puts "How many children do you have?"
-children[:children] = gets.chomp
+interior_design[:children] = gets.chomp.to_i
 
-puts "Which theme would you like: "
-puts "Texas Country Theme"
-texas_theme = gets.chomp
-puts "Gulf Coast Beach Theme"
-gulf_theme = gets.chomp
-puts "Colorado Moutain Theme"
-Colorado_theme = gets.chomp
-puts "West Coast Theme"
-west_coast_theme = gets.chomp
+puts "Which theme would you like? (Please answer when prompted.)"
+puts "Texas Country Theme (yes or no):"
+interior_design[:texas] = theme(gets.chomp)
+puts "Gulf Coast Beach Theme (yes or no):"
+interior_design[:gulf] = theme(gets.chomp)
+puts "Colorado Moutain Theme (yes or no):"
+interior_design[:colorado] = theme(gets.chomp)
+puts "Southern California Theme (yes or no):"
+interior_design[:west_coast] = theme(gets.chomp)
+
+# Print results 
+
+p interior_design
+
+# Modify any data
+
+puts "Would you like to make any changes or updates?"
+
