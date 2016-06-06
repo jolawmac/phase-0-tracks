b3418e8a8b576e404751e69a4ec03965ec72e6f3
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-06 13:21:06 -0500

    Add hash to get name data

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+# Get spies real name
+# Swap first and last name
+# Change vowels to next vowel using .next
+# Change consonants to next consonant using .next
+
+name = {}
+
+puts "Please enter your first name:"
+name[:first_name] = gets.chomp
+
+puts "Please enter your last name:"
+name[:last_name] = gets.chomp
+
+p name 
