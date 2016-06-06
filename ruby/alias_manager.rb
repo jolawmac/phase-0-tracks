a079a3a180769cd39b8b12f37e6f3baa7636d685
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-06 18:51:47 -0500

    Add if else statements to first name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,34 @@
-# Get spies real name
+# Ask for the spies first and last name that they would like disguised.
 # Swap first and last name
 # Change vowels to next vowel using .next
 # Change consonants to next consonant using .next
+# Print the results 
 
-name = {}
+# Vowels and consonants hash
+
+vowels = "aeiou".split('')
+
+# Vowels should be "a" => "e", etc, and "u" => "a"
+
+consonants = "bcdfghjklmnpqrstvwxyz".split('')
+
+# Consonants should be "b" => "c", etc, and "z" => "b"
+
+
+# Asking for names:
 
 puts "Please enter your first name:"
-name[:first_name] = gets.chomp
+first_name = gets.chomp.downcase.split('')
+undercover_name = first_name.each 
+	if vowels != "u"
+		vowels = letters.next
+	elsif vowels == "u"
+		vowels = "a"
+		
+
 
 puts "Please enter your last name:"
-name[:last_name] = gets.chomp
+last_name = gets.chomp.downcase.split('')
 
-p name 
+
+ p undercover_name
