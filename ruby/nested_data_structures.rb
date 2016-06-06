a68b911151fdf9e933b2f229ca11e3f7afa75afc
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-05 22:21:52 -0500

    Add nested data structure hashes and arrays

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,47 @@
+hospice_inpatient_unit = {
+	room1: {
+		patient_name: 'Patient 1',
+		room_equipment1: {
+			bed: 1,
+			lamp: 1,
+			chair: 1,
+			med_box: 2,
+			oxygen: 1
+		},
+		equipment_needed1: [ 
+			"More meds",
+			"Toilet paper",
+			"Pillow"
+		]
+
+	},
+	room2: {
+		patient_name: 'Patient 2'
+		room_equipment2: {
+			bed: 1,
+			lamp: 1,
+			nightstand: 1,
+			med_box: 1,
+			wheelchair: 1
+			}
+		equipment_needed2: [
+			"Morphine",
+			"Xxygen",
+			"Speical Diet"
+		]
+	}, 
+	room3: {
+		patient_name: 'New_Patient'
+		room_equipment3: {
+			bed: 2,
+			chair_bed: 1,
+			lamp: 1,
+			tv: 1,
+			nightstand: 1,
+			med_box: 2,
+			oxygen: 1
+		},
+		equipment_needed3: []
+	}
+
+}
