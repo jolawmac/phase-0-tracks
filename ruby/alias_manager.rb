51ca855fa417d40f89f01b6d5961529948accf05
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-07 08:25:02 -0500

    Delete first name last name and add name gets chomp

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,8 @@
 # Ask the agent for their name:
-# Swap the first and last names using .split(' ') and .reverse!
+# Swap the first and last names:
 
-puts "Please enter your name:"
-name = gets.chomp.downcase.split(' ').reverse!
-
-# Change the vowels in the names
+puts "Please enter your full name:"
+name = gets.chomp.downcase.split(' ').reverse
 
 def new_vowels(name)
 	name.map! do |vowel|
@@ -13,4 +11,22 @@ def new_vowels(name)
 end
 
 
+def new_consonants(name)
+	name.map! do |consonants|
+		consonants.gsub(/[bcdfghjklmnpqrstvwxyz]/, "b" => "c", "c" => "d", "d" => "f", "f" => "g",	"g" => "h", "h" => "j", "j" => "k", "k" => "l", "l" => "m", "m" => "n", "n" => "p", "p" => "q", "q" => "r", "r" => "s", "s" => "t", "t" => "v", "v" => "w",	"w" => "x",	"x" => "y", "y" => "z", "z" => "b")
+	end
+end
 
+name = new_vowels(name)
+name = new_consonants(name)
+
+
+def cap_letters(new_name)
+	new_name.map do |capitalize|
+		capitalize.capitalize
+	end
+end
+
+new_name = cap_letters(name).join(" ")
+
+puts "Your new hidden identity is: #{new_name}"
