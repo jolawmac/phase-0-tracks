537fdf10f5a480b5e5959a9ad5252fb9e7a7dc14
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-07 07:31:36 -0500

    Delete old code and start new add gsub

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,34 +1,16 @@
-# Ask for the spies first and last name that they would like disguised.
-# Swap first and last name
-# Change vowels to next vowel using .next
-# Change consonants to next consonant using .next
-# Print the results 
+# Ask the agent for their name:
+# Swap the first and last names using .split(' ') and .reverse!
 
-# Vowels and consonants hash
+puts "Please enter your name:"
+name = gets.chomp.downcase.split(' ').reverse!
 
-vowels = "aeiou".split('')
+# Change the vowels in the names
 
-# Vowels should be "a" => "e", etc, and "u" => "a"
-
-consonants = "bcdfghjklmnpqrstvwxyz".split('')
-
-# Consonants should be "b" => "c", etc, and "z" => "b"
+def new_vowels(name)
+	name.map! do |vowel|
+		vowel.gsub(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+	end
+end
 
 
-# Asking for names:
 
-puts "Please enter your first name:"
-first_name = gets.chomp.downcase.split('')
-undercover_name = first_name.each 
-	if vowels != "u"
-		vowels = letters.next
-	elsif vowels == "u"
-		vowels = "a"
-		
-
-
-puts "Please enter your last name:"
-last_name = gets.chomp.downcase.split('')
-
-
- p undercover_name
