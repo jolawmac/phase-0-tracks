64bd7b02182b4818741cdd60bb72da936060b671
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-08 00:15:31 -0500

    Update comment section

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,9 +1,26 @@
 # Question 2
 
 def calculate(x,y,z)
-	x.to_i
-	y = x + z
-	z.to_i
+	x = x.to_i
+	y = " "
+	z = z.to_i
 end
 
 calculate(1, '+', 5)
+# Not able to figure out how to add arguments xy & z withint my method. 
+
+# It took me a while to figure out methods in the previous lessons. 
+# Not enough time on this lesson to figure it out. 
+# I think the last two weeks have gone very quick and not enough time to fully grasp methods. 
+# I need more time!
+
+
+# I know eventually I'll need something like the following:
+#	when :+ then puts x + z
+#   when :- then puts x - z
+#   when :* then puts x * z
+#   when :/ then puts x / z
+#   when :% then puts x % z
+#   else
+#   puts "invalid input"
+#  	end
