6c406883c3285b24ba5931c32a59f6191b7e4f7a
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-11 11:38:40 -0500

    Add comment section at the bottom of the file

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -93,4 +93,56 @@ update_item_from_list(new_list, "IceCream",1)
 prettify_item_from_list(new_list)
 
 
-#
+# REFLECT: 
+# What did you learn about pseudocode from working on this challenge?
+# I learned how important pseudocode is to writing out the actual code.
+# Pseudocode makes writing the code out so much easier. It helps formulate
+# and organize thoughts. Makes the work much easier and quicker. It also helps
+# with finding errors. Creates a to do list.
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# It seems to me that using a hash was much easier for this exercise. I also 
+# feel like hashes are used much more than arrays in coding because hashes 
+# have both keys and values as opposed to the array's single list of items. 
+# Once we figured out how to call the key and/or value to modify them 
+# hashes seem so much easier to work with.
+
+# What does a method return?
+# The method returns whatever commmands are within the method itself. 
+# Methods return the value of the last executed command. 
+# Parameters allow the method to access data outside the method. These need
+# to be access within the method. So when we created the method to capitalize 
+# an item inside our hash, we accessed the data outside the method from within the 
+# method by using the argument grocery_list. It then returns a modified 
+# version once its called after the method. 
+
+# What kind of things can you pass into methods as arguments?
+# All kinds of things. Strings, integers, etc. Depends on the code inside the 
+# method as well. The arugement is used inside the method to complete the action in the code.
+# It is then called after the method to execute that code.
+
+# How can you pass information between methods?
+# Once the arugments are set if needed and the code inside the method is written out
+# the information is passed by simply calling the method outside the method below it.
+# Example is: 
+# def method1(list)
+# end
+# method(list) 
+# Using method(list) will pass the info through the method.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# This challege helped in so many ways. I think weeks 4 and 5 were very overwhelming. So much 
+# information in such a short time. My biggest issue until this lesson was methods. 
+# I had issues with method syntax. This challenge really helped me understand 
+# using methods, how to write them out, and in which order they should be written.
+# I think I still need a little practice but I feel so much more confident now
+# in writing methods. Hands down one of the best lessons we've done so far.
+# Not much confusion now. The only thing I'm stuggling with now is which commands 
+# to use inside the methods to accomplish what I want done. I think research can 
+# help clear that up for me. 
+
+
+
+
+
+
