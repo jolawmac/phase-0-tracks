87a9f5d0d5e0fbf33ba2bd8e8aed86121b514b39
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-12 14:30:28 -0500

    Add initialization commands and empty array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,19 @@
 class Santa
 
-	def initialize 
+	def initialize(gender, ethnicity)
 		p "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity	
+		@accessories = accessories
+	end
+
+	def deer_arr(arr)
+		arr = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end 
+
+	def age(int)
+		int += 0
+		puts "Age: #{integer}"
 	end
 
 	def speak 
@@ -12,3 +24,23 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end 
 end 
+
+santas []
+
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
