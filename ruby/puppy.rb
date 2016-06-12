e5a81552791d4093edab92a5366f224b9cfec0a9
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-11 21:00:38 -0500

    Add puppy class in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Puppy Class:
+# TYPE OF PUPPY
+# Beagle 
+
+# CHARACTERISTICS
+# Fur color: brown
+# Hunter: yes
+# Learns quick: yes
+# Type of bark: medium
+
+# BEHAVIOR
+# Fetches
+# Playful
+# Friendly 
