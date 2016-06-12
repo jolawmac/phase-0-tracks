4267890792d109e22687103d7a85eec85e6c1f4f
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-12 13:30:44 -0500

    Add class and initialize method to file

---
+++ ruby/santa.rb
@@ -0,0 +1,14 @@
+class Santa
+
+	def initialize 
+		p "Initializing Santa instance..."
+	end
+
+	def speak 
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end 
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+	end 
+end 
