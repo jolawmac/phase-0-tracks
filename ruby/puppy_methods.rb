b29660d4a12913b7556e53c5736c64792c2c34af
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-12 13:23:23 -0500

    Add pairing data to file

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,42 @@
 class Puppy
+  attr_reader  :talk_dog
+  attr_accessor :nick_name, :fetch, :speak, :dog_years
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+def initialize(nick_name,fetch, speak, dog_years)
+  @nick_name = nick_name
+  @fetch = fetch
+  @speak = speak
+  @dog_years = dog_years
   end
 
+  def talk_dog
+    @nick_name + " " + @fetch + " " + @speak + " " + @dog_years
+    end
+  def speak_fifty(x)
+    @speak * 50
+  end
 end
 
+#   def fetch
+#     @fetch 
+#   end
+
+#   def speak
+#     @speak
+#   end
+
+#   def dog_years
+#     @dog_years
+    
+#   end
+# end
+puppy = Puppy.new("spot","ball", "woof", "ten")
+puts puppy.talk_dog
+puts puppy.speak_fifty(2)
+puppy.nick_name = "sasha"
+puts puppy.nick_name
+
+
+# puts puppy.fetch + " " +
+#    puppy.speak + " " +
+#    puppy.dog_years 
