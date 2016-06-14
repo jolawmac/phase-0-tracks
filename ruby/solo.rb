285a7806b0db68bdd515128477c85257c3a46cea
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-14 06:57:08 -0500

    Add methods to file

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,7 +1,41 @@
-# DRAGON:
-# Three attributes: country_of_origin, age, breathes_fire
-# Three methods: age goes up, change country of origin, breathes fire
-# Define class
-# Insert intialize method
-# Use attr_reader and attr_accessor 
-# 
+
+class Car
+
+	# attr_reader :year_model :speed
+	# attr_accessor :company :type
+
+	def initialize(company, type, engine_sound)
+		@company = company
+		@type = type
+		@year_model = 0
+		@speed = 0
+		@engine_sound = engine_sound
+	end 
+
+	def company 
+		puts "The car company you like is: #{@company}"
+	end
+	
+	def engine_sound
+		puts "#{@engine_sound}!"
+	end
+	
+	 def speed (new_speed)
+	   if new_speed == 0
+		  puts "You have stopped."
+	 	elsif new_speed > @speed
+    	  puts "You increased to #{new_speed} mph."
+    	elsif new_speed < @speed
+    	  puts "You decreased to #{new_speed} mph."
+    	else
+    	 puts "You are traveling at #{@speed} mph."
+     	end
+    	 @speed = new_speed
+	 end
+	
+end
+
+
+# test code 
+car = Car.new("Ford", "Mustang", "Vroom")
+car.speed(25)
