6ebb3dbbc37755c868d20a9d54cd8f0b9825b06b
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-13 19:36:14 -0500

    Sudo code

---
+++ ruby/solo.rb
@@ -0,0 +1,7 @@
+# DRAGON:
+# Three attributes: country_of_origin, age, breathes_fire
+# Three methods: age goes up, change country of origin, breathes fire
+# Define class
+# Insert intialize method
+# Use attr_reader and attr_accessor 
+# 
