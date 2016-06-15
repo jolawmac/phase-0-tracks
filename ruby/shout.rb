3bda274865d39666c41a773de023347283dd5bd5
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-14 22:12:28 -0500

    Add module and class methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,35 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
 
-	def self.yelling_happily(words)
-		words + ":D" + ":D"
-	end 
+# 	def self.yelling_happily(words)
+# 		words + ":D" + ":D"
+# 	end 
+# end
+
+# # Driver Code
+
+# p Shout.yell_angrily("Blah")
+# p Shout.yelling_happily("YAY")
+
+module Shout
+	def yell(scream)
+		puts "You are yelling #{scream}!"
+	end
 end
 
-# Driver Code
+class Happy
+	include Shout
+end
 
-p Shout.yell_angrily("Blah")
-p Shout.yelling_happily("YAY")
+class Angry
+	include Shout
+end 
+
+happy = Happy.new
+happy.yell("YAY :)")
+
+angry = Angry.new
+angry.yell("GRRRR >:(")
+
