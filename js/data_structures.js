bdda020c1564a2beb32b34b9d6b482082de9583c
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-18 11:50:07 -0500

    Update file with loop method

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,27 @@
 var colors = ["blue", "orange", "red", "green"];
 
-var horseNames = ["Gimpy", "Ed", "Lightening", "Margret"]
+var horseNames = ["Gimpy", "Ed", "Lightening", "Margret"];
 
-colors.push("purple")
 
-horseNames.push("Satin")
+colors.push("purple");
+//console.log(colors);
+
+horseNames.push("Satin");
+//console.log(horseNames);
+
+
+// Create empty object (var name = {})
+// Var will be index of array that will reference a place in the array 
+// Var index will the index of the horse array that we'll push into key of new object
+// Another var that will reference index of colors into value of new object 
+
+function assignHorse(horses, horseColors) {
+	var coloredHorses = {};
+	for (var i = 0; i < horses.length; i++) {
+		coloredHorses[horses[i]] = horseColors[i];
+		//coloredHorses[horseColors[i]] = horseColors[i];
+	}
+	console.log(coloredHorses);
+}
+
+assignHorse(horseNames, colors);
