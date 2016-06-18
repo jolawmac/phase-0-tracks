73c02ff2d60f773cb7ef5192f491a0f7c21070f0
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-18 12:37:44 -0500

    Add constructor function to file

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,13 +15,26 @@ horseNames.push("Satin");
 // Var index will the index of the horse array that we'll push into key of new object
 // Another var that will reference index of colors into value of new object 
 
-function assignHorse(horses, horseColors) {
-	var coloredHorses = {};
-	for (var i = 0; i < horses.length; i++) {
-		coloredHorses[horses[i]] = horseColors[i];
-		//coloredHorses[horseColors[i]] = horseColors[i];
-	}
-	console.log(coloredHorses);
-}
+// function assignHorse(horses, horseColors) {
+// 	var coloredHorses = {};
+// 	for (var i = 0; i < horses.length; i++) {
+// 		coloredHorses[horses[i]] = horseColors[i];
+// 		//coloredHorses[horseColors[i]] = horseColors[i];
+// 	}
+// 	console.log(coloredHorses);
+// }
 
-assignHorse(horseNames, colors);
+// assignHorse(horseNames, colors);
+
+
+function car(make, fourDoor) {
+	console.log("Our new car: "+ make + ", Four door: "+ fourDoor);
+};
+
+function drives() {
+		return "Vroom, vroom!"
+	};
+
+newCar = new car("Dodge", true);
+newCar.drives;
+
