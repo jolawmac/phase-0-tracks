77dfb1a49fd347890c2c864e7632f3fd7c72afc2
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-20 21:06:23 -0500

    Add driver code

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,28 @@
 // Pseudocode:
-// 1. Make an empty variable using function and the variable name
-// 2. Print the variable using console.log 
-// 3. When printing the variable use the correct java code to reverse the string.
-// 4. 
+// 1. Make a variable using function which takes a string as an argument.
+// 2. When returning the variable use the correct javascript code to reverse the string.
+// 3. Call it into action!
 
-function myString().reverse().toString() {
-	console.log("Hello!");
+
+// First try: 
+// function myString().reverse().toString() {
+// 	console.log("Hello!");
+// }
+
+// myString();
+
+
+// Second try:
+function reverseString(str) {
+	return str.split("").reverse().join("");
 }
 
-myString();
+// Test Driver Code:
+reverseString("Hello");
+
+// Driver Code:
+var newString = reverseString("Hi, my name is Josh!");
+console.log(newString);
+newString;
+
+
