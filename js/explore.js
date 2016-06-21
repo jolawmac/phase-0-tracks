ec4dee332546cd1e34e4d7ed5d040b85f55231c0
Joshua McDonald <joshua_mcdonald@me.com>
2016-06-20 21:24:01 -0500

    Add driver code

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,10 @@
 // Pseudocode:
 // 1. Make a variable using function which takes a string as an argument.
-// 2. When returning the variable use the correct javascript code to reverse the string.
-// 3. Call it into action!
+// 2. When returning the variable use the argument with the correct javascript code to reverse the string.
+// 		a. Use split to split the string apart
+//		b. Use reverse to revese the string
+//		c. Use join to bring the string back togethere
+// 3. Call it into action using driver code.
 
 
 // First try: 
@@ -20,9 +23,20 @@ function reverseString(str) {
 // Test Driver Code:
 reverseString("Hello");
 
-// Driver Code:
+// Driver Code 1:
 var newString = reverseString("Hi, my name is Josh!");
 console.log(newString);
 newString;
 
+// Driver Code 2:
+var truthString = true;
+
+function thisIsTrue(){
+	if (truthString) {
+		console.log(newString);
+	}
+	else {
+		console.log(false);
+	}
+}
 
