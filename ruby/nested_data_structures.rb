14b85cdd8c31275248d625a36a310ffeec5320b7
Joshua McDonald <joshua_mcdonald@me.com>
2016-07-24 21:08:02 -0500

    Add code to print data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -48,15 +48,42 @@ hospice_inpatient_unit = {
 
 }
 
+
+
+# Iterating through nested data structure: 
+
+
+
 # Printed nested data
 
-hospice_inpatient_unit[:room1][:room_equipment1][2]
+# hospice_inpatient_unit[:room1][:equipment_needed1][2]
 
-hospice_inpatient_unit[:room3][:equipment_needed3].push("Med cups")
 
-hospice_inpatient_unit[:room2][:patient_name]
 
-hospice_inpatient_unit[:room3][:room_equipment3].delete(2)
+# Adding items to a nested data structure: 
 
-hospice_inpatient_unit[:room2][:equipment_needed2].reverse[2]
+# hospice_inpatient_unit[:room3][:equipment_needed3].push("Med cups")
+# hospice_inpatient_unit[:room3][:equipment_needed3].push("Oxygen")
+
+# p hospice_inpatient_unit[:room3][:equipment_needed3]
+
+
+
+# Calling data in an array in a nested data structure with arrays and hashes: 
+
+#  hospice_inpatient_unit[:room2][:patient_name]
+
+
+
+# Deletes an item inside the hash
+
+# hospice_inpatient_unit[:room3][:room_equipment3].delete(:lamp)
+ 
+# puts hospice_inpatient_unit[:room3][:room_equipment3]
+
+
+
+# Simply reverses the order of items in the array 
+
+# hospice_inpatient_unit[:room2][:equipment_needed2].reverse
 
