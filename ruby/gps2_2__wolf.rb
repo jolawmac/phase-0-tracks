bd53173888bc186ad1866790de9ca827bf5e05c4
Joshua McDonald <joshua_mcdonald@me.com>
2016-07-27 20:53:53 -0500

    Update with sudo code

---
+++ ruby/gps2_2__wolf.rb
@@ -0,0 +1,37 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Create an empty hash
+  # set default quantity
+  # Split the string into an array
+  # Each item will the key and the quanitity will the value 
+  # Push items into the hash 
+  # print the list to the console [can you use one of your other methods here?]
+# output: Hash 
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+	# Create a method with two parameters 
+	# Add each key and value to the end of the list
+# output: The hash with the new key value pair included with data already in hash.
+
+# Method to remove an item from the list
+# input: Item name
+# steps:
+	# Delete the key value pair from the cooresponding item name 
+# output: Print new hash without delete item 
+
+# Method to update the quantity of an item
+# input: Item name and the new value 
+# steps: 
+	# Located the key value pair and update to the new value 
+# output: Print new hash with updated value of an item 
+
+# Method to print a list and make it look pretty
+# input: The information in the hash. No parameter needed. Calling the hash
+# steps: 
+	# Iterate over the hash with line break and semicolons 
+# output: New pretty hash
+
+
