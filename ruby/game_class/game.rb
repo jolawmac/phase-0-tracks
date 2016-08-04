a4c1bbe8c4e0e545b3abacfabcabf19fc1c961eb
Joshua McDonald <joshua_mcdonald@me.com>
2016-08-04 08:52:44 -0500

    Create files

---
+++ ruby/game_class/game.rb
@@ -0,0 +1,23 @@
+# CLASS:
+class Game
+
+	attr_reader 
+	attr_accessor 
+
+	def initialize 
+		@word
+		@guess_count = 0
+		@is_over = false
+	end 
+
+	def word_length
+		@guess_count += 1
+		if 
+	end 
+
+	def 
+	end 
+
+end 
+
+# DRIVER CODE: 
